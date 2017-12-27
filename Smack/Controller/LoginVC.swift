5f57cdc01f6891b6c5fab6dcd782c52ab812c002
fa473 <fa473@users.noreply.github.com>
2017-12-27 13:54:56 -0800

    create login page

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by home on 12/27/17.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by home on 12/27/17.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
+
