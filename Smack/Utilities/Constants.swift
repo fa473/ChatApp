6e756bb00fa4d12a7019a657cc98d4837a77a2cb
fa473 <fa473@users.noreply.github.com>
2017-12-30 22:33:22 -0800

    login functionality

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "http://localhost:3005/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
 // Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5046553938)
@@ -36,4 +37,8 @@ let USER_EMAIL = "userEmail"
 // Headers
 let HEADER = [
     "Content-Type": "application/json; charset=utf-8"
+]
+let BEARER_HEADER = [
+    "Authorization":"Bearer \(AuthService.instance.authToken)",
+    "Content-Type":"application/json; charset=utf-8"
 ]
